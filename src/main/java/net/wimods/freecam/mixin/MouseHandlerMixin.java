8259b247c8fa22b821e64d8031ffc1e91ffe6f20
Alexander01998 <contact.wurstimperium@gmail.com>
2026-06-02 17:15:15 +0200

    Update to 26.2-pre-3

--- src/main/java/net/wimods/freecam/mixin/MouseHandlerMixin.java
+++ src/main/java/net/wimods/freecam/mixin/MouseHandlerMixin.java
@@ -30,7 +30,7 @@ public abstract class MouseHandlerMixin
 	
 	@WrapWithCondition(method = "onScroll(JDD)V",
 		at = @At(value = "INVOKE",
-			target = "Lnet/minecraft/world/entity/player/Inventory;setSelectedSlotDeferred(I)V"))
+			target = "Lnet/minecraft/world/entity/player/Inventory;setSelectedSlot(I)V"))
 	private boolean wrapOnScroll(Inventory inventory, int slot)
 	{
 		return !WiFreecam.INSTANCE.isControllingScrollEvents();
